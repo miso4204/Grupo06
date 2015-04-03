a486ce8a4229afb98827638f8b1c24d8bfcd3d5e
caespinosam <ca.espinosa12@uniandes.edu.co>
2015-04-03 15:36:24 -0500

    Se agrega objeto request para el  servicio rest de creación de Producto.

--- proyecto/web-app/src/main/java/grupo6/web/controller/rest/ProductoRestController.java
+++ proyecto/web-app/src/main/java/grupo6/web/controller/rest/ProductoRestController.java
@@ -2,6 +2,7 @@ package grupo6.web.controller.rest;
 
 import grupo6.modulo.product.service.view.IProductoService;
 import grupo6.persistencia.entidades.Producto;
+import grupo6.web.dto.ProductoRequestDTO;
 
 import java.util.Date;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,9 +38,15 @@ public class ProductoRestController {
 	@RequestMapping(value = "/crear", method = RequestMethod.POST, 
 						consumes = MediaType.APPLICATION_JSON_VALUE,
 						produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Long crearProducto(@RequestBody Producto producto) {
+	public @ResponseBody Long crearProducto(@RequestBody ProductoRequestDTO productoDTO) {
 		
-		producto.setFechaInicioPublicacion(new Date());
+		Producto producto = new Producto();
+		producto.setCiudad(productoDTO.getCiudad());
+		producto.setLugar(productoDTO.getLugar());
+		producto.setNombre(productoDTO.getNombre());
+		producto.setPrecio(productoDTO.getPrecio());
+		producto.setUrlImagen(productoDTO.getUrlImagen());
+		producto.setFechaInicio(productoDTO.getFechaInicio());
 		producto.setFechaCreacion(new Date());
 		return productoService.crearProducto(producto); 
 	}
@@ -53,6 +61,22 @@ public class ProductoRestController {
 	public @ResponseBody List<Producto> listarProductos() {
 		
 		return productoService.listarTodosProductos();
+	}
+	
+	
+	/**
+	 * Servicio REST para buscar productos por precio.
+	 * 
+	 * @return los productos del sistema filtrados por precio en formato JSON.
+	 */
+	@RequestMapping(value = "/buscar_por_precio/{precioInicial}/{precioFinal}",
+			method = RequestMethod.GET, 
+						produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Producto> listarPorPrecio(
+			@PathVariable("precioInicial") Double precioInicial,
+			@PathVariable("precioFinal") Double precioFinal) {
+		
+		return productoService.buscarProductosPorPrecio(precioInicial, precioFinal);
 	}
 
 }
---
+++ proyecto/web-app/src/main/java/grupo6/web/dto/ProductoRequestDTO.java
@@ -0,0 +1,85 @@
+package grupo6.web.dto;
+
+import grupo6.web.json.serializer.JsonDateSerializer;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+/**
+ * DTO que represneta un Producto creado a partir de un JSON.
+ * 
+ * @author caespinosam
+ * 
+ */
+public class ProductoRequestDTO implements Serializable {
+
+	/**
+	 * UID generado.
+	 */
+	private static final long serialVersionUID = -8658863654377821942L;
+
+	/** Nombre del paquete producto. Ej: paquete semana santa. **/
+	private String nombre;
+	/** Lugar. ej: Finca xxx **/
+	private String lugar;
+	/** Ciudad. Ej: Medellin **/
+	private String ciudad;
+	/** Precio del paquete en COP. **/
+	private Double precio;
+	/** Fecha inicio en que se debe tomar el paquete. **/
+	private Date fechaInicio;
+	/** Las fotos del producto. **/
+	private String urlImagen;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	@JsonSerialize(using = JsonDateSerializer.class)
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public String getUrlImagen() {
+		return urlImagen;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public void setUrlImagen(String urlImagen) {
+		this.urlImagen = urlImagen;
+	}
+
+}
