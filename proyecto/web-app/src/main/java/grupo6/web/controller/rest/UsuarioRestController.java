f279b3e4c3c1bb67ece7ce7b133f46d5d8d30ab3
jhon0010 <jhoning.soft@gmail.com>
2015-04-03 16:02:19 -0500

    Dependencia pom a servlet para manejo de sessión controller del usuario

---
+++ proyecto/web-app/src/main/java/grupo6/web/controller/rest/UsuarioRestController.java
@@ -0,0 +1,120 @@
+package grupo6.web.controller.rest;
+
+import grupo6.modulo.user.service.impl.IUsuarioService;
+import grupo6.persistencia.entidades.Usuario;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Controlador REST de los servicios del producto.
+ * @author jhon
+ *
+ */
+@Controller 
+@RequestMapping("/usuario")
+public class UsuarioRestController {
+
+	@Autowired 
+	private IUsuarioService usuarioService;//Inyeccion servicios
+	
+	@Autowired
+	private HttpSession httpSession; // Usado para manejar la seesion del usuario
+
+	public static final String ROL_SESSION = "rol";
+	public static final String USER_NAME_SESSION = "userName";
+	public static final String USER_ID_SESSION = "userId";
+	
+	/**
+	 * Servicio REST que permite identificar un usuario en el sistema 
+	 * y deja sus datos principales en session.
+	 * @param nombre de usuario y password
+	 * 
+	 * @return el true si es valido y false si no o si ocurre un error
+	 */
+	@SuppressWarnings("unused")
+	@RequestMapping(value = "/login", method = RequestMethod.POST, 
+						consumes = MediaType.APPLICATION_JSON_VALUE,
+						produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody boolean login(@RequestBody String usuario, String password) {
+		
+		try {
+			
+			Usuario  user = usuarioService.ingresar(usuario, password);
+			httpSession.setAttribute(USER_ID_SESSION, user.getId());  
+			httpSession.setAttribute(ROL_SESSION, user.getRol());
+			httpSession.setAttribute(USER_NAME_SESSION, user.getUsuario());
+			
+			if (user != null) {
+				return true;
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+	
+	/**
+	 * Servicio REST que crea un usuario.
+	 * @param Usuario a crear. Creado a partir de un JSON.
+	 * 
+	 * @return el id del nuevo usuario.
+	 */
+	@RequestMapping(value = "/crear", method = RequestMethod.POST, 
+						consumes = MediaType.APPLICATION_JSON_VALUE,
+						produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody long crearUsuario(@RequestBody Usuario user) {
+		
+		return usuarioService.crearUsuario(user);
+	}
+	
+	
+	/**
+	 * Servicio REST que acvtualiza la infor de un usuario.
+	 * @param Usuario a actualizar. actualizado a partir de un JSON.
+	 * 
+	 * @return true si actualiza exitosamente y false si no
+	 */
+	@RequestMapping(value = "/crear", method = RequestMethod.POST, 
+						consumes = MediaType.APPLICATION_JSON_VALUE,
+						produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody boolean actualizarUsuario(@RequestBody Usuario user) {
+		
+		return usuarioService.actualizarUsuario(user);
+	}
+	
+	
+	/**
+	 * Servicio REST que apermite cambiar la contrasennia de un usuario
+	 * @param Pasword anterior a actualizar. actualizado a partir de un JSON.
+	 * 
+	 * @return true si actualiza exitosamente y false si no
+	 */
+	@RequestMapping(value = "/change_pass", method = RequestMethod.POST, 
+						consumes = MediaType.APPLICATION_JSON_VALUE,
+						produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody boolean cambiarContrasennia(@RequestBody String passActual, @RequestBody String passNuevo, 
+			@RequestBody String passNuevoValidate) {
+		
+		@SuppressWarnings("unused")
+		String mensajeError = ""; // MOSTRAR EN UN MODAL
+		
+		try {
+			String userName = (String) httpSession.getAttribute(USER_NAME_SESSION);
+			return usuarioService.cambiarPassword(userName, passActual, passNuevo, passNuevoValidate);
+		} catch (Exception e) {
+			mensajeError = e.getMessage();
+			return false;
+		}
+	}
+}
